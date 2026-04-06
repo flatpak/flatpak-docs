62841bfa466644a84de7959897ecf0f8f29d1f69
bbhtt <bbhtt.zn0i8@slmail.me>
2026-04-06 16:08:19 +0530

    conf: Ignore launchpad.net links
    Times out in GitHub CI

--- docs/conf.py
+++ docs/conf.py
@@ -165,6 +165,7 @@ linkcheck_ignore = [
     r"https://www\.gnu\.org/.*",             # Broken in GitHub actions
     r"https://www\.freedesktop\.org/.*",     # Uses Anubis so get "418 Client Error: I'm a teapot for url: ..."
     r"https://gitlab\.gnome\.org/.*",        # 406 probably due to Anubis
+    r"https?://launchpad\.net/.*",           # Timeout in GitHub actions
 ]
 
 linkcheck_request_headers = {
